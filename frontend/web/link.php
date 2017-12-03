f843ddb68be5e6b8a23f2e9e72bcfd5f8768e61c
at-ducnguyen <duc.nguyen@asiantech.vn>
2017-12-03 11:49:56 +0700

    Upload Database

--- frontend/web/link.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-
-<link rel="stylesheet" href="css/bootstrap-theme.min.css">
-<link rel="stylesheet" href="css/res.css">
-<script src="js/bootstrap.min.js"></script>
-<script src="js/jquery.min.js"></script>
-<script src="js/res.js"></script>
--- frontend/web/slider.php
+++
@@ -1,8 +0,0 @@
-<?php
-use frontend\widgets\SliderWidget;
-?>
-
- <header style="margin-top: -100px">
-<?php SliderWidget::begin(); ?>
-<?php SliderWidget::end(); ?>
-</header>
